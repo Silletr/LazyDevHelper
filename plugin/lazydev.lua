eb3d54f3630942c3a5343a3238174bf7bc5722ac
silletr <hristicenkoigor764@gmail.com>
2025-06-24 22:04:13 +0300

    added the get_installed_libs function to pip check file

--- plugin/lazydev.lua
+++ plugin/lazydev.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_user_command("is work", function()
   print("Yep!")
 end, {})
-
+--  Creating function with getting all libraries (executing pip list on ../python/pip_check.py.py) 
 vim.api.nvim_api_create_user_command("SuggerImports", function(opts)
     
   local prefix = opts.args or ""
