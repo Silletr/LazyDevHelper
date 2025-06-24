b87c612486f9c1b0aaaf711dd9401f0d599c1e17
Silletr <153998312+Silletr@users.noreply.github.com>
2025-06-24 21:04:40 +0300

    Update lazydev.lua

--- plugin/lazydev.lua
+++ plugin/lazydev.lua
@@ -1,3 +1,19 @@
 vim.api.nvim_create_user_command("is work", function()
   print("Yep!")
 end, {})
+
+vim.api.nvim_api_create_user_command("SuggerImports", function(opts)
+    
+  local prefix = opts.args or ""
+
+  local cmd = string.format("python3 ~/.config/python/pip_check.py %s", prefix)
+  local handle = io.popen(cmd)
+  local result = handle:read("*a")
+  handle:close()
+
+  if result == "" then
+    print("No matches 🤔")
+  else:
+    print("Variants: \n", result)
+  end
+end, { nargs = "?" })
