05e57ed67504222ae7bb90ef39ae97028ab6fc17
silletr <hristicenkoigor764@gmail.com>
2025-06-24 22:16:28 +0300

    Added lazy dev helper to the lua`s folder (before: nvim / main_folder_name

---
+++ LazyDevHelper/plugin/lazydev.lua
@@ -0,0 +1,22 @@
+vim.api.nvim_create_user_command("is work", function()
+  print("Yep!")
+end, {})
+--  Creating function with getting all libraries (executing pip list on ../python/pip_check.py.py) 
+  --
+vim.api.nvim_create_user_command("SuggestImports", function(opts)
+    local prefix = opts.args or ""
+    
+    local cmd = string.format("python3 ~/.config/python/pip_check.py %s", prefix)
+    local handle = io.popen(cmd)
+    local result = handle:read("*a")
+    handle:close()
+
+    if result == "" then
+        print("No matches 🤔")
+    else
+        print("Variants: \n", result)
+    end
+end, { nargs = "?" })
+
+vim.api.nvim_exec('command', true)
+vim.cmd("SuggestImports")
