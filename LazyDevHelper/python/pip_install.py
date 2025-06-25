9395bfc7983f2b3364ee964bef3aa89557954991
silletr <hristicenkoigor764@gmail.com>
2025-06-25 19:46:02 +0300

    modified:   plugin/commands.lua 	modified:   plugin/lazydev.lua 	modified:   python/pip_install.py

--- LazyDevHelper/python/pip_install.py
+++ LazyDevHelper/python/pip_install.py
@@ -1,15 +1,36 @@
+print(">>> pip_install started <<<")
 import subprocess
 from pip_check import get_installed_libs
+import sys
 
 
-def install_lib():
-    installed_libs = get_installed_libs()
-    if installed_libs:
-        print(f"Found {len(installed_libs)} installed packages:")
-        for lib in installed_libs:
-            print(f"- {lib}")
-    else:
-        print("No packages found")
+def install_lib(lib_name: str):
+    print(f"📦 Installing {lib_name} ...")
+    try:
+        result = subprocess.run(
+            ["pip3", "install", lib_name],
+            capture_output=False,
+            text=True,
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        )
+        print(result.stdout)
 
+        if result.stderr:
+            print(result.stderr)
 
-install_lib()
+    except subprocess.CalledProcessError as e:
+        print(f"❌ Failed to install {lib_name}")
+        print(e.stderr)
+
+def main():
+    if len(sys.argv) < 2:
+        print("⚠️ Provide at least one lib")
+        return
+
+    for lib in sys.argv[1:]:
+        install_lib(lib)
+
+if __name__ == "__main__":
+    main()
