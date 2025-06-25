4b0d2b7060fb5664fdbc6bbb66e475b7650a7637
silletr <hristicenkoigor764@gmail.com>
2025-06-25 12:03:31 +0300

    Added pre commit file, modified and added pip install file to future work

--- LazyDevHelper/python/pip_install.py
+++ LazyDevHelper/python/pip_install.py
@@ -1,5 +1,15 @@
 import subprocess
-from python import pip_check
+from python.pip_check import get_installed_libs
 
 def install_lib():
-    lib_name = 
+    installed_libs = get_installed_libs()
+    if installed_libs:
+        print(f"Found {len(installed_libs)} installed packages:")
+        for lib in installed_libs:
+            print(f"- {lib}")
+    else:
+        print("No packages found")
+
+
+if __name__ == "__main__":
+    install_lib()
